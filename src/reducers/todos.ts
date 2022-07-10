d45e4d3991810259d23885d945f3c6573967d036
Nik Stern <sirocknick@gmail.com>
2022-07-10 15:38:13 -0700

    Going to santa barbera

---
+++ src/reducers/todos.ts
@@ -0,0 +1,11 @@
+import  { FetchTodosActions, Todo } from '../actions'
+import { ActionTypes } from '../actions/types'
+
+export const todosReducer = (state: Todo[] = [], action: FetchTodosActions) => {
+  switch(action.type) {
+    case ActionTypes.fetchTodos: 
+      return action.payload;
+    default:
+      return state;
+  }
+}
